c0f54d809175cd226c7bbe995e1cb79cd9f8ac02
Martin Haesemeyer <m.haesemeyer@gmail.com>
2017-08-14 18:29:15 -0400

    Inital project commit

---
+++ core.py
@@ -0,0 +1,7 @@
+#  Copyright 2017 Martin Haesemeyer. All rights reserved.
+#
+# Licensed under the MIT license
+
+"""
+Commonly used helper functions
+"""
---
+++ mixedInputModel.py
@@ -0,0 +1,8 @@
+#  Copyright 2017 Martin Haesemeyer. All rights reserved.
+#
+# Licensed under the MIT license
+
+"""
+Builds a gradient prediction network that uses a mixed input layer, i.e. the input is a 2D tensor of both behavior and
+temperature variables on which convolution filters operate jointly
+"""
