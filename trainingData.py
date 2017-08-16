88a0a174b6fc2569bcc1d42839d502645aab7b81
Martin Haesemeyer <m.haesemeyer@gmail.com>
2017-08-16 12:12:27 -0400

    Wrote code to create training dataset from simulated trajectory

--- trainingData.py
+++ trainingData.py
@@ -44,7 +44,9 @@ class GradientData:
     """
     Class that represents training/test data from a gradient experiment
     """
-    pass
+    def __init__(self, model_in, model_out):
+        self.model_in_raw = model_in
+        self.model_out_raw = model_out
 
 
 class GradientSimulation:
@@ -88,7 +90,8 @@ class GradientSimulation:
         self.__trn_cash = RandCash(1000, lambda s: np.random.randn(s) * self.sd_trn + self.mu_trn)
         self.__uni_cash = RandCash(1000, lambda s: np.random.rand(s))
         # place holder to receive bout trajectories for efficiency
-        self.__bout = np.empty((self.blen, 3))
+        self.__bout = np.empty((self.blen, 3), np.float32)
+        self.__pos_cache = np.empty((self.nsteps, 3), np.float32)
 
     def temperature(self, r):
         """
@@ -143,22 +146,83 @@ class GradientSimulation:
         Forward run of random gradient exploration
         :return: The position and heading in the gradient at each timepoint
         """
-        all_pos = np.zeros((self.nsteps, 3))
-        i = 1
-        while i < self.nsteps:
+        return self.sim_forward(self.nsteps, np.zeros(3), "N").copy()
+
+    def sim_forward(self, nsteps, start_pos, start_type):
+        """
+        Simulates a number of steps ahead
+        :param nsteps: The number of steps to perform
+        :param start_pos: The current starting conditions [x,y,a]
+        :param start_type: The behavior to perform on the first step "N", "S", "L", "R"
+        :return: The position at each timepoint nsteps*[x,y,a]
+        """
+        if start_type not in ["N", "S", "L", "R"]:
+            raise ValueError("start_type has to be either (N)o bout, (S)traight, (L)eft or (R)right")
+        if self.__pos_cache.shape[0] != nsteps:
+            self.__pos_cache = np.zeros((nsteps, 3))
+        all_pos = self.__pos_cache
+        if start_type == "N":
+            all_pos[0, :] = start_pos
+            i = 1
+        else:
+            traj = self.get_bout_trajectory(start_pos, start_type)
+            if traj.size <= nsteps:
+                all_pos[:traj.shape[0], :] = traj
+                i = traj.size
+            else:
+                return traj[:nsteps, :]
+        while i < nsteps:
             dec = self.__uni_cash.next_rand()
             if dec < self.p_move:
                 bt = self.get_bout_type()
-                traj = self.get_bout_trajectory(all_pos[i-1, :], bt)
-                if i+self.blen <= self.nsteps:
-                    all_pos[i:i+self.blen, :] = traj
+                traj = self.get_bout_trajectory(all_pos[i - 1, :], bt)
+                if i + self.blen <= nsteps:
+                    all_pos[i:i + self.blen, :] = traj
                 else:
                     all_pos[i:, :] = traj[:all_pos[i:, :].shape[0], :]
                 i += self.blen
             else:
-                all_pos[i, :] = all_pos[i-1, :]
+                all_pos[i, :] = all_pos[i - 1, :]
                 i += 1
         return all_pos
+
+    def create_dataset(self, sim_pos):
+        """
+        Creates a GradientData object by executing all behavioral choices at simulated positions in which the fish
+        was stationary
+        :param sim_pos: Previously created simulation trajectory
+        :return: GradientData object with all necessary training in- and outputs
+        """
+        if sim_pos.shape[1] != 3:
+            raise ValueError("sim_pos has to be nx3 array with xpos, ypos and heading at each timepoint")
+        history = FRAME_RATE * HIST_SECONDS
+        start = history + 1  # start data creation with enough history present
+        # initialize model inputs and outputs
+        inputs = np.zeros((sim_pos.shape[0] - start, 3, history), np.float32)
+        outputs = np.zeros((sim_pos.shape[0] - start, 4), np.float32)
+        btypes = ["N", "S", "L", "R"]
+        # create vector that tells us when the fish was moving
+        all_dx = np.r_[0, np.diff(sim_pos[:, 0])]
+        is_moving = all_dx != 0
+        # loop over each position, simulating PRED_WINDOW into future to obtain real finish temperature
+        for step in range(start, sim_pos.shape[0]):
+            if is_moving[step]:
+                continue
+            # obtain inputs at given step
+            r = np.sqrt(np.sum(sim_pos[step-history+1:step+1, 0:2]**2, 1))
+            inputs[step-start, 0, :] = self.temperature(r)
+            spd = np.sqrt(np.sum(np.diff(sim_pos[step-history:step+1, 0:2], axis=0)**2, 1))
+            inputs[step-start, 1, :] = spd
+            inputs[step-start, 2, :] = np.diff(sim_pos[step-history:step+1, 2], axis=0)
+            # select each possible behavior in turn starting from this step and simulate
+            # PRED_WINDOW steps into the future to obtain final temperature as output
+            for i, b in enumerate(btypes):
+                fpos = self.sim_forward(PRED_WINDOW, sim_pos[step, :], b)[-1, :]
+                outputs[step-start, i] = self.temperature(np.sqrt(fpos[0]**2 + fpos[1]**2))
+        # create gradient data object on all non-moving positions
+        is_moving = is_moving[start:]
+        assert is_moving.size == inputs.shape[0]
+        return GradientData(inputs[np.logical_not(is_moving), :, :], outputs[np.logical_not(is_moving), :])
 
 
 if __name__ == '__main__':
