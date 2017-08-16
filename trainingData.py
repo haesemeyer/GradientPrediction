2c086f40062f392d2583bdaf25266eebd46938f2
Martin Haesemeyer <m.haesemeyer@gmail.com>
2017-08-15 20:57:58 -0400

    Wrote code to perform random swim simulation in the gradient to obtain trajectory

--- trainingData.py
+++ trainingData.py
@@ -54,12 +54,18 @@ class GradientSimulation:
     500 ms into the future for each selectable behavior but only one path
     will actually be chosen to advance the simulation to avoid massive branching
     """
-    def __init__(self, nsteps: int):
+    def __init__(self, nsteps: int, radius, t_min, t_max):
         """
         Creates a new GradientSimulation
         :param nsteps: The number of steps to perform
+        :param radius: The arena radius in mm
+        :param t_min: The center temperature
+        :param t_max: The edge temperature
         """
         self.nsteps = nsteps
+        self.radius = radius
+        self.t_min = t_min
+        self.t_max = t_max
         # Create 3D array to hold all model inputs derived during the simulation
         self.model_inputs = np.empty((nsteps, 3, FRAME_RATE*HIST_SECONDS), dtype=np.float32)
         # Create 2D array to hold all possible final temperatures during the simulation
@@ -67,19 +73,92 @@ class GradientSimulation:
         # Set bout parameters used in the simulation
         self.p_move = 1.0 / FRAME_RATE  # Bout frequency of 1Hz on average
         self.blen = int(FRAME_RATE * 0.2)  # Bouts happen over 200 ms length
+        self.bfrac = np.linspace(0, 1, self.blen)
         # Displacement is drawn from gamma distribution
         self.disp_k = 2.63
         self.disp_theta = 1 / 0.138
         # Turn angles of straight swims and turns are drawn from gaussian
-        self.mu_str = 0
-        self.sd_str = 2
-        self.mu_trn = 30
-        self.sd_trn = 5
-        # set up cashes of random numbers for bout parameters
-        self.__disp_cash = RandCash(1000, lambda s: np.random.gamma(self.disp_k, self.disp_theta, s))
+        self.mu_str = np.deg2rad(0)
+        self.sd_str = np.deg2rad(2)
+        self.mu_trn = np.deg2rad(30)
+        self.sd_trn = np.deg2rad(5)
+        # set up cashes of random numbers for bout parameters - divisor for disp is for conversion to mm
+        self.__disp_cash = RandCash(1000, lambda s: np.random.gamma(self.disp_k, self.disp_theta, s) / 9)
         self.__str_cash = RandCash(1000, lambda s: np.random.randn(s) * self.sd_str + self.mu_str)
         self.__trn_cash = RandCash(1000, lambda s: np.random.randn(s) * self.sd_trn + self.mu_trn)
         self.__uni_cash = RandCash(1000, lambda s: np.random.rand(s))
+        # place holder to receive bout trajectories for efficiency
+        self.__bout = np.empty((self.blen, 3))
+
+    def temperature(self, r):
+        """
+        Returns the temperature at the given radius
+        """
+        return (r / self.radius) * (self.t_max - self.t_min) + self.t_min
+
+    def get_bout_type(self):
+        """
+        With 1/3 probability for each type returns a random bout type
+        """
+        dec = self.__uni_cash.next_rand()
+        if dec < 1.0/3:
+            return "S"
+        elif dec < 2.0/3:
+            return "L"
+        else:
+            return "R"
+
+    def get_bout_trajectory(self, start, bout_type="S"):
+        """
+        Gets a trajectory for the given bout type
+        :param start: Tuple/vector of x, y, angle at start of bout
+        :param bout_type: The type of bout: (S)traight, (L)eft turn, (R)ight turn
+        :return: The trajectory of the bout (blen rows, 3 columns: x, y, angle)
+        """
+        if bout_type == "S":
+            da = self.__str_cash.next_rand()
+        elif bout_type == "L":
+            da = -1 * self.__trn_cash.next_rand()
+        elif bout_type == "R":
+            da = self.__trn_cash.next_rand()
+        else:
+            raise ValueError("bout_type has to be one of S, L, or R")
+        heading = start[2] + da
+        disp = self.__disp_cash.next_rand()
+        dx = np.cos(heading) * disp * self.bfrac
+        dy = np.sin(heading) * disp * self.bfrac
+        # reflect bout if it would take us outside the dish
+        r_end = np.sqrt((start[0]+dx)**2 + (start[1]+dy)**2)[-1]
+        if r_end > self.radius:
+            heading = heading + np.pi
+            dx = np.cos(heading) * disp * self.bfrac
+            dy = np.sin(heading) * disp * self.bfrac
+        self.__bout[:, 0] = dx + start[0]
+        self.__bout[:, 1] = dy + start[1]
+        self.__bout[:, 2] = heading
+        return self.__bout
+
+    def run_simulation(self):
+        """
+        Forward run of random gradient exploration
+        :return: The position and heading in the gradient at each timepoint
+        """
+        all_pos = np.zeros((self.nsteps, 3))
+        i = 1
+        while i < self.nsteps:
+            dec = self.__uni_cash.next_rand()
+            if dec < self.p_move:
+                bt = self.get_bout_type()
+                traj = self.get_bout_trajectory(all_pos[i-1, :], bt)
+                if i+self.blen <= self.nsteps:
+                    all_pos[i:i+self.blen, :] = traj
+                else:
+                    all_pos[i:, :] = traj[:all_pos[i:, :].shape[0], :]
+                i += self.blen
+            else:
+                all_pos[i, :] = all_pos[i-1, :]
+                i += 1
+        return all_pos
 
 
 if __name__ == '__main__':
