07816e422c788e819df0031eb44d08c3afc553df
Martin Haesemeyer <m.haesemeyer@gmail.com>
2017-08-20 16:38:42 -0400

    Added saving of model every 5000 steps. Added plots to display squared error loss and rank loss across training.

--- trainingGround.py
+++ trainingGround.py
@@ -11,6 +11,7 @@ from trainingData import GradientData
 import numpy as np
 import matplotlib.pyplot as pl
 import seaborn as sns
+from scipy.ndimage import gaussian_filter1d
 
 BATCHSIZE = 50
 
@@ -19,13 +20,25 @@ if __name__ == "__main__":
     trainingData = GradientData.load("gd_training_data.hdf5")
     train_losses = []
     rank_errors = []
+    # store our training operation
+    tf.add_to_collection('train_op', mIM.t_step)
+    # create saver
+    saver = tf.train.Saver(max_to_keep=None)
     with tf.Session() as sess:
         sess.run(tf.global_variables_initializer())
-        for i in range(50000):
+        for i in range(100000):
+            # save naive model including full graph
+            if i == 0:
+                save_path = saver.save(sess, "mixedInputModel", global_step=i)
+                print("Model saved in file: %s" % save_path)
+            # save variables every 5000 steps but don't re-save model-meta
+            if i != 0 and i % 5000 == 0:
+                save_path = saver.save(sess, "mixedInputModel", global_step=i, write_meta_graph=False)
+                print("Model saved in file: %s" % save_path)
             batch_data = trainingData.training_batch(BATCHSIZE)
             xbatch = batch_data[0]
             ybatch = batch_data[1]
-            cur_l = mIM.total_loss.eval(feed_dict={mIM.x_in: xbatch, mIM.y_: ybatch, mIM.keep_prob: 1.0})
+            cur_l = mIM.sq_loss.eval(feed_dict={mIM.x_in: xbatch, mIM.y_: ybatch, mIM.keep_prob: 1.0})
             # compare ranks of options in prediction vs. ranks of real options
             pred = mIM.m_out.eval(feed_dict={mIM.x_in: xbatch, mIM.y_: ybatch, mIM.keep_prob: 1.0})
             sum_rank_diffs = 0.0
@@ -37,7 +50,7 @@ if __name__ == "__main__":
             rank_errors.append(sum_rank_diffs / BATCHSIZE)
             if i % 200 == 0:
                 print('step %d, training loss %g, rank loss %g' % (i, cur_l, sum_rank_diffs/BATCHSIZE))
-            mIM.t_step.run(feed_dict={mIM.x_in: xbatch, mIM.y_: ybatch, mIM.keep_prob: mIM.DROP_TRAIN})
+            mIM.t_step.run(feed_dict={mIM.x_in: xbatch, mIM.y_: ybatch, mIM.keep_prob: mIM.KEEP_TRAIN})
         weights_conv1 = mIM.W_conv1.eval()
 
     w_ext = np.max(np.abs(weights_conv1))
@@ -47,3 +60,17 @@ if __name__ == "__main__":
     for i, a in enumerate(ax):
         sns.heatmap(weights_conv1[:, :, 0, i], ax=a, vmin=-w_ext, vmax=w_ext, center=0, cbar=False)
         a.axis("off")
+
+    pl.figure()
+    pl.plot(train_losses, 'o')
+    pl.plot(np.arange(len(train_losses)), gaussian_filter1d(train_losses, 25))
+    pl.xlabel("Batch")
+    pl.ylabel("Training loss")
+    sns.despine()
+
+    pl.figure()
+    pl.plot(rank_errors, 'o')
+    pl.plot(np.arange(len(rank_errors)), gaussian_filter1d(rank_errors, 25))
+    pl.xlabel("Batch")
+    pl.ylabel("Avg. rank error")
+    sns.despine()
