cf303f8f4460736620ea5b72481ff4ef1e2df25f
Martin Haesemeyer <m.haesemeyer@gmail.com>
2017-08-23 15:06:04 -0400

    Made frequency of train and test evaluations proper constants instead of magic values. Write losses to file.

--- trainingGround.py
+++ trainingGround.py
@@ -12,9 +12,13 @@ import numpy as np
 import matplotlib.pyplot as pl
 import seaborn as sns
 from scipy.ndimage import gaussian_filter1d
+import h5py
 
 BATCHSIZE = 50
 TESTSIZE = 200
+
+EVAL_TRAIN_EVERY = 5  # every this many trials training set performance is evaluated
+EVAL_TEST_EVERY = 1000  # every this many trials test set performance is evaluated
 
 if __name__ == "__main__":
     import mixedInputModel as mIM
@@ -50,7 +54,7 @@ if __name__ == "__main__":
             xbatch = batch_data[0]
             ybatch = batch_data[1]
             # every five steps compute training losses
-            if i % 5 == 0:
+            if i % EVAL_TRAIN_EVERY == 0:
                 cur_l = mIM.sq_loss.eval(feed_dict={mIM.x_in: xbatch, mIM.y_: ybatch, mIM.keep_prob: 1.0})
                 # compare ranks of options in prediction vs. ranks of real options
                 pred = mIM.m_out.eval(feed_dict={mIM.x_in: xbatch, mIM.y_: ybatch, mIM.keep_prob: 1.0})
@@ -64,7 +68,7 @@ if __name__ == "__main__":
                 if i % 200 == 0:
                     print('step %d, training loss %g, rank loss %g' % (i, cur_l, sum_rank_diffs/BATCHSIZE))
             # every 1000 steps run test
-            if i % 1000 == 0:
+            if i % EVAL_TEST_EVERY == 0:
                 test = testData.training_batch(TESTSIZE)
                 xtest = test[0]
                 ytest = test[1]
@@ -94,19 +98,28 @@ if __name__ == "__main__":
     fig.savefig("ConvolutionFilters.pdf", type="pdf")
 
     fig = pl.figure()
-    pl.plot(np.arange(len(train_losses)) * 5, train_losses, 'o', alpha=0.2)
-    pl.plot(np.arange(len(train_losses)) * 5, gaussian_filter1d(train_losses, 25))
-    pl.plot(np.arange(len(test_losses)) * 1000, test_losses, 'o')
+    pl.plot(np.arange(len(train_losses)) * EVAL_TRAIN_EVERY, train_losses, 'o', alpha=0.2)
+    pl.plot(np.arange(len(train_losses)) * EVAL_TRAIN_EVERY, gaussian_filter1d(train_losses, 25))
+    pl.plot(np.arange(len(test_losses)) * EVAL_TEST_EVERY, test_losses, 'o')
     pl.xlabel("Batch")
     pl.ylabel("Training/Test loss")
     sns.despine()
     fig.savefig("SquaredLoss.pdf", type="pdf")
 
     fig = pl.figure()
-    pl.plot(np.arange(len(rank_errors)) * 5, rank_errors, 'o', alpha=0.2)
-    pl.plot(np.arange(len(rank_errors)) * 5, gaussian_filter1d(rank_errors, 25))
-    pl.plot(np.arange(len(test_rank_errors)) * 1000, test_rank_errors, 'o')
+    pl.plot(np.arange(len(rank_errors)) * EVAL_TRAIN_EVERY, rank_errors, 'o', alpha=0.2)
+    pl.plot(np.arange(len(rank_errors)) * EVAL_TRAIN_EVERY, gaussian_filter1d(rank_errors, 25))
+    pl.plot(np.arange(len(test_rank_errors)) * EVAL_TEST_EVERY, test_rank_errors, 'o')
     pl.xlabel("Batch")
     pl.ylabel("Avg. rank error")
     sns.despine()
     fig.savefig("RankError.pdf", type="pdf")
+
+    # save loss evaluations to file
+    with h5py.File("./model_data/losses.hdf5", "x") as dfile:
+        dfile.create_dataset("train_eval", data=np.arange(len(train_losses)) * EVAL_TRAIN_EVERY)
+        dfile.create_dataset("train_losses", data=train_losses)
+        dfile.create_dataset("test_eval", data=len(test_losses) * EVAL_TEST_EVERY)
+        dfile.create_dataset("test_losses", data=test_losses)
+        dfile.create_dataset("train_rank_errors", data=rank_errors)
+        dfile.create_dataset("test_rank_errors", data=test_rank_errors)
