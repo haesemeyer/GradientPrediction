7760d8d2f4b201440b95db91b57c46ffd1bdd633
Martin Haesemeyer <m.haesemeyer@gmail.com>
2017-11-24 17:03:36 -0500

    Added linear gradient training class. Now run three simulations and combine into one TrainingData object by default

--- trainingData.py
+++ trainingData.py
@@ -7,7 +7,7 @@ Module to generate, save and load training and test data and provide access to t
 """
 
 import numpy as np
-from core import TrainingSimulation
+from core import TrainingSimulation, GradientData
 
 
 class CircGradientTrainer(TrainingSimulation):
@@ -48,6 +48,53 @@ class CircGradientTrainer(TrainingSimulation):
         return r > self.radius
 
 
+class LinGradientTrainer(TrainingSimulation):
+    """
+    Class for generating training data in a linear gradient
+    """
+    def __init__(self, xmax, ymax, t_min, t_max):
+        """
+        Creates a new LinerGradient simulation
+        :param xmax: The maximal x-coordinate (gradient direction)
+        :param ymax: The maximal y-coordinate (neutral direction)
+        :param t_min: The minimum temperature at left edge
+        :param t_max: The maximal temperature at right edge
+        """
+        super().__init__()
+        self.xmax = xmax
+        self.ymax = ymax
+        self.t_min = t_min
+        self.t_max = t_max
+
+    def temperature(self, x, y):
+        """
+        Returns the temperature at the given positions
+        """
+        return (x / self.xmax) * (self.t_max - self.t_min) + self.t_min
+
+    def out_of_bounds(self, x, y):
+        """
+        Detects whether the given x-y position is out of the arena
+        :param x: The x position
+        :param y: The y position
+        :return: True if the given position is outside the arena, false otherwise
+        """
+        if x < 0 or x > self.xmax:
+            return True
+        if y < 0 or y > self.ymax:
+            return True
+        return False
+
+    def run_simulation(self, nsteps):
+        """
+        Forward run of random gradient exploration
+        :param nsteps: The number of steps to simulate
+        :return: The position and heading in the gradient at each timepoint
+        """
+        spos = np.array([self.xmax // 2, self.ymax // 2, 0])
+        return self.sim_forward(nsteps, spos, "N").copy()
+
+
 if __name__ == '__main__':
     import matplotlib.pyplot as pl
     import seaborn as sns
@@ -57,7 +104,7 @@ if __name__ == '__main__':
     if response == "y":
         n_steps = int(input("Number of steps to perform?"))
         gradsim = CircGradientTrainer(100, 22, 37)
-        print("Running gradient simulation")
+        print("Running radial simulation, inside-out")
         pos = gradsim.run_simulation(n_steps)
         pl.figure()
         pl.plot(pos[:, 0], pos[:, 1])
@@ -66,4 +113,31 @@ if __name__ == '__main__':
         sns.despine()
         print("Generating gradient data")
         grad_data = gradsim.create_dataset(pos)
+        all_in = grad_data.model_in_raw
+        all_out = grad_data.model_out_raw
+        print("Running radial simulation, outside-in")
+        gradsim = CircGradientTrainer(100, 37, 22)
+        pos = gradsim.run_simulation(n_steps)
+        pl.figure()
+        pl.plot(pos[:, 0], pos[:, 1])
+        pl.xlabel("X position [mm]")
+        pl.ylabel("Y position [mm]")
+        sns.despine()
+        print("Generating gradient data")
+        grad_data = gradsim.create_dataset(pos)
+        all_in = np.r_[all_in, grad_data.model_in_raw]
+        all_out = np.r_[all_out, grad_data.model_out_raw]
+        print("Running linear simulation")
+        gradsim = LinGradientTrainer(200, 50, 22, 37)
+        pos = gradsim.run_simulation(n_steps)
+        pl.figure()
+        pl.plot(pos[:, 0], pos[:, 1])
+        pl.xlabel("X position [mm]")
+        pl.ylabel("Y position [mm]")
+        sns.despine()
+        print("Generating gradient data")
+        grad_data = gradsim.create_dataset(pos)
+        all_in = np.r_[all_in, grad_data.model_in_raw]
+        all_out = np.r_[all_out, grad_data.model_out_raw]
+        grad_data = GradientData(all_in, all_out)
         print("Done")
