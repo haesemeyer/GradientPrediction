5705dbe377505a71e8eea8bc49f0b848412e5648
Martin Haesemeyer <m.haesemeyer@gmail.com>
2018-04-09 09:22:40 -0400

    Added t and m branch retraining navigation and cell type fractions

--- Figure4.py
+++ Figure4.py
@@ -19,7 +19,8 @@ import h5py
 from global_defs import GlobalDefs
 from pandas import DataFrame
 import pickle
-from data_stores import SimulationStore
+from data_stores import SimulationStore, ActivityStore
+from multiprocessing import Pool
 
 
 # file definitions
@@ -29,9 +30,85 @@ paths_512_zf = [f + '/' for f in os.listdir(base_path_zf) if "_3m512_" in f]
 base_path_ce = "./model_data/CE_Adam_1e-4/"
 paths_512_ce = [f + '/' for f in os.listdir(base_path_ce) if "_3m512_" in f]
 
+_mpool = None
+
 
 def mpath(base, path):
     return base + path[:-1]  # need to remove trailing slash
+
+
+def shutdown_pool():
+    global _mpool
+    if _mpool is not None:
+        _mpool.close()
+        _mpool.join()
+        _mpool = None
+
+
+def get_pool():
+    global _mpool
+    if _mpool is None:
+        cp_count = os.cpu_count()
+        if cp_count is None:
+            cp_count = 2
+        _mpool = Pool(cp_count // 2)
+    return _mpool
+
+
+def get_best_fit(activities, regressors):
+    """
+    For each cell in activities returns the index of the best fitting regressor >0.6 or -1 if none found
+    Correlations are performed after calcium convolution
+    :param activities: n_timepoints x m_cells matrix of cell activations
+    :param regressors: n_timepoints x k_regressors matrix of regressors
+    :return: m long vector of best fit regressor indices
+    """
+    # build calcium convolution kernel
+    tau_on = 1.4  # seconds
+    tau_on *= GlobalDefs.frame_rate  # in frames
+    tau_off = 2  # seconds
+    tau_off *= GlobalDefs.frame_rate  # in frames
+    kframes = np.arange(10 * GlobalDefs.frame_rate)  # 10 s long kernel
+    kernel = 2**(-kframes / tau_off) * (1 - 2**(-kframes / tau_on))
+    kernel = kernel / kernel.sum()
+    fit_vec = np.zeros(activities.shape[1])
+    for i in range(activities.shape[1]):
+        corrs = np.empty(regressors.shape[1])
+        for j in range(corrs.size):
+            corrs[j] = np.corrcoef(c.ca_convolve(activities[:, i], 0, 0, kernel),
+                                   c.ca_convolve(regressors[:, j], 0, 0, kernel))[0, 1]
+        corrs[np.isnan(corrs)] = 0
+        if corrs.max() < 0.6:
+            fit_vec[i] = -1
+        else:
+            fit_vec[i] = np.argmax(corrs)
+    return fit_vec
+
+
+def get_cluster_assignments(mt: MoTypes, model_dir: str, regressors, t_stimulus, std, droplist):
+    """
+    Creates a dictionary of cluster assignments for cells in t and m branch of a model
+    :param mt: The model organism to use
+    :param model_dir: The folder of the model checkpoint
+    :param regressors: The cluster regressors
+    :param t_stimulus: The temperature stimulus to use
+    :param std: The standardizations
+    :param droplist: Unit drop list
+    :return: Dictionary with 't' and 'm' unit responses to stimulus
+    """
+    md = c.ModelData(model_dir)
+    ml = mt.network_model()
+    ml.load(md.ModelDefinition, md.LastCheckpoint)
+    # prepend lead-in to stimulus
+    lead_in = np.full(ml.input_dims[2] - 1, np.mean(t_stimulus[:10]))
+    temp = np.r_[lead_in, t_stimulus]
+    act_dict = ml.unit_stimulus_responses(temp, None, None, std, droplist)
+    mpool = get_pool()
+    ares = {k: [mpool.apply_async(get_best_fit, (ad, regressors)) for ad in act_dict[k]] for k in ['t', 'm']}
+    retval = {k: np.vstack([ar.get() for ar in ares[k]]) for k in ares}
+    # ares = {k: mpool.apply_async(get_best_fit, (np.hstack(act_dict[k]), regressors)) for k in ['t', 'm']}
+    # retval = {k: ares[k].get() for k in ['t', 'm']}
+    return retval
 
 
 if __name__ == "__main__":
@@ -73,10 +150,13 @@ if __name__ == "__main__":
 
     # get activity data
     all_ids_zf = []
+    all_cells_zf = []
     for i, p in enumerate(paths_512_zf):
         cell_res, ids = ana_zf.temperature_activity(mpath(base_path_zf, p), temperature, i)
         all_ids_zf.append(ids)
+        all_cells_zf.append(cell_res)
     all_ids_zf = np.hstack(all_ids_zf)
+    all_cells_zf = np.hstack(all_cells_zf)
     all_ids_ce = []
     for i, p in enumerate(paths_512_ce):
         cell_res, ids = ana_ce.temperature_activity(mpath(base_path_ce, p), temperature, i)
@@ -114,7 +194,6 @@ if __name__ == "__main__":
                 for j in range(len(paths_512_zf)*2):
                     rand_clusts[j * nw_units:j * nw_units + int(nw_units * ptr / 100)] = 1
                 dlist = a.create_det_drop_list(i, rand_clusts, all_ids_zf, [1], True)
-                print("Desired: {0}, actual 1: {1}, actual 2: {2}".format(ptr, 100*dlist['t'][0].sum()/512, 100*dlist['t'][1].sum()/512))
                 dlist_file.create_dataset(file_key, data=np.void(pickle.dumps(dlist, pickle.HIGHEST_PROTOCOL)))
             pos = ana_zf.run_simulation(mp, "r", "bfevolve", drop_list=dlist)
             rem_d["values"].append(a.preferred_fraction(pos, "r", 1.0))
@@ -264,28 +343,117 @@ if __name__ == "__main__":
     ax.set_yticks([0, 0.25, 0.5, 0.75])
     fig.savefig(save_folder + "ce_type_ablations.pdf", type="pdf")
 
-    # panel 6: Gradient distribution after fish-like ablations and re-training
+    # for each removal and retrain compute gradient distributions
     trained = np.empty((len(paths_512_zf), centers.size))
     fl_ablated = np.empty_like(trained)
-    fl_retrained = np.empty_like(trained)
+    fl_retrained_t = np.empty_like(trained)
+    fl_retrained_nont = np.empty_like(trained)
+    nfl_ablated = np.empty_like(trained)
     for i, p in enumerate(paths_512_zf):
         mp = mpath(base_path_zf, p)
-        rt_path = mp + "/fl_retrain"
+        rt_path_nont = mp + "/fl_nontbranch_retrain"
+        rt_path_t = mp + "/fl_tbranch_retrain"
         with SimulationStore(None, std_zf, MoTypes(False)) as sim_store:
             pos = sim_store.get_sim_pos(mp, 'r', "trained")
             trained[i, :] = a.bin_simulation(pos, bns, 'r')
             dlist = a.create_det_drop_list(i, clust_ids_zf, all_ids_zf, [1, 2, 3, 4, 5])
             pos = sim_store.get_sim_pos(mp, 'r', "trained", dlist)
             fl_ablated[i, :] = a.bin_simulation(pos, bns, 'r')
-            pos = sim_store.get_sim_pos(rt_path, 'r', "trained", dlist)
-            fl_retrained[i, :] = a.bin_simulation(pos, bns, 'r')
+            pos = sim_store.get_sim_pos(rt_path_t, 'r', "trained", dlist)
+            fl_retrained_t[i, :] = a.bin_simulation(pos, bns, 'r')
+            pos = sim_store.get_sim_pos(rt_path_nont, 'r', "trained", dlist)
+            fl_retrained_nont[i, :] = a.bin_simulation(pos, bns, 'r')
+            dlist = a.create_det_drop_list(i, clust_ids_zf, all_ids_zf, [0, 6, 7])
+            pos = sim_store.get_sim_pos(mp, 'r', "trained", dlist)
+            nfl_ablated[i, :] = a.bin_simulation(pos, bns, 'r')
+
+    # panel 6: Consequence of ablating all fish or non-fish types
     fig, ax = pl.subplots()
     sns.tsplot(trained, centers, n_boot=1000, condition="Trained", color="k")
-    sns.tsplot(fl_ablated, centers, n_boot=1000, condition="Ablated", color="C1")
-    sns.tsplot(fl_retrained, centers, n_boot=1000, condition="Retrained", color="C3")
+    sns.tsplot(fl_ablated, centers, n_boot=1000, condition="Fish-like ablated", color="C1")
+    sns.tsplot(nfl_ablated, centers, n_boot=1000, condition="Non-fish ablated", color="C3")
     ax.plot([GlobalDefs.tPreferred, GlobalDefs.tPreferred], [0, 0.03], 'k--', lw=0.25)
     ax.legend()
     ax.set_xlabel("Temperature [C]")
     ax.set_ylabel("Proportion")
     sns.despine(fig, ax)
-    fig.savefig(save_folder + "zf_ablation_and_retrain_distribution.pdf", type="pdf")
+    fig.savefig(save_folder + "zf_fish_nonfish_ablation_distribution.pdf", type="pdf")
+
+    # panel 7: Retraining after ablating all fish types
+    fig, ax = pl.subplots()
+    sns.tsplot(fl_ablated, centers, n_boot=1000, condition="Ablated", color="k")
+    sns.tsplot(trained, centers, n_boot=1000, condition="Trained", color='k')
+    sns.tsplot(fl_retrained_t, centers, n_boot=1000, condition="Temperature part retrained", color="C1")
+    sns.tsplot(fl_retrained_nont, centers, n_boot=1000, condition="Shared part retrained", color="C3")
+    ax.plot([GlobalDefs.tPreferred, GlobalDefs.tPreferred], [0, 0.03], 'k--', lw=0.25)
+    ax.legend()
+    ax.set_xlabel("Temperature [C]")
+    ax.set_ylabel("Proportion")
+    sns.despine(fig, ax)
+    fig.savefig(save_folder + "zf_fish_retrained_distribution.pdf", type="pdf")
+
+    # panel 8: Type fractions pre-ablation, post-ablation and after full re-training
+    # make cluster regressors
+    regs_zf = np.zeros((all_cells_zf.shape[0], np.unique(clust_ids_zf).size - 1))
+    for cnum in np.unique(clust_ids_zf):
+        if cnum == -1:
+            continue
+        regs_zf[:, cnum] = np.mean(all_cells_zf[:, clust_ids_zf == cnum], 1)
+    type_fractions = {"cell type": [], "state": [], "fraction": [], "network_id": [], "branch": []}
+    # get cell responses and populate our fractions
+    for i, p in enumerate(paths_512_zf):
+        mp = mpath(base_path_zf, p)
+        rt_path_nont = mp + "/fl_nontbranch_retrain"
+        rt_path_t = mp + "/fl_tbranch_retrain"
+        with ActivityStore(None, std_zf, MoTypes(False)) as act_store:
+            clusters_trained = get_cluster_assignments(MoTypes(False), mp, regs_zf, temperature, std_zf, None)
+            dlist = a.create_det_drop_list(i, clust_ids_zf, all_ids_zf, [1, 2, 3, 4, 5])
+            clusters_ablated = get_cluster_assignments(MoTypes(False), mp, regs_zf, temperature, std_zf, dlist)
+            clusters_retr_t = get_cluster_assignments(MoTypes(False), rt_path_t, regs_zf, temperature, std_zf, dlist)
+            clusters_retr_nont = get_cluster_assignments(MoTypes(False), rt_path_nont, regs_zf, temperature, std_zf,
+                                                         dlist)
+            for cnum in np.unique(clust_ids_zf):
+                for br in ['t', 'm']:
+                    if cnum == 4:
+                        ctype = "Fast ON"
+                    elif cnum == 5:
+                        ctype = "Slow ON"
+                    elif cnum == 1:
+                        ctype = "Fast OFF"
+                    elif cnum == 3:
+                        ctype = "Slow OFF"
+                    elif cnum == 2:
+                        ctype = "Int. OFF"
+                    elif cnum == -1:
+                        ctype = "Unassigned"
+                    else:
+                        ctype = "Non fish"
+                    type_fractions["cell type"] += [ctype]*4
+                    type_fractions["branch"] += [br]*4
+                    type_fractions["network_id"] += [i]*4
+                    type_fractions["state"].append("Trained")
+                    type_fractions["fraction"].append(np.sum(clusters_trained[br] == cnum)/clusters_trained[br].size)
+                    type_fractions["state"].append("Ablated")
+                    type_fractions["fraction"].append(np.sum(clusters_ablated[br] == cnum) / clusters_ablated[br].size)
+                    type_fractions["state"].append("Retrained T")
+                    type_fractions["fraction"].append(np.sum(clusters_retr_t[br] == cnum) / clusters_retr_t[br].size)
+                    type_fractions["state"].append("Retrained M")
+                    type_fractions["fraction"].append(np.sum(clusters_retr_nont[br] == cnum) /
+                                                      clusters_retr_nont[br].size)
+    df_typefrac = DataFrame(type_fractions)
+    order = ["Fast ON", "Slow ON", "Fast OFF", "Slow OFF", "Int. OFF", "Non fish", "Unassigned"]
+    # panel 9: type fractions after ablation and re-training in temperature branch
+    fig, ax = pl.subplots()
+    sns.barplot(x="cell type", y="fraction", hue="state", data=df_typefrac[df_typefrac["branch"] == 't'], ci=68,
+                order=order)
+    sns.despine(fig, ax)
+    fig.savefig(save_folder + "zf_ablation_and_retrain_tbranch_tfracs.pdf", type="pdf")
+
+    # panel 10: type fractions after ablation and re-training in mixed branch
+    fig, ax = pl.subplots()
+    sns.barplot(x="cell type", y="fraction", hue="state", data=df_typefrac[df_typefrac["branch"] == 'm'], ci=68,
+                order=order)
+    sns.despine(fig, ax)
+    fig.savefig(save_folder + "zf_ablation_and_retrain_mbranch_tfracs.pdf", type="pdf")
+
+    shutdown_pool()
