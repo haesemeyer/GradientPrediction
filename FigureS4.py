894c98f3dcd5a9958a101b904bae21b460990ce0
Martin Haesemeyer <m.haesemeyer@gmail.com>
2018-06-09 12:19:34 -0400

    Added panels for all cluster responses, cluster fractions and connectivity

--- FigureS4.py
+++ FigureS4.py
@@ -18,6 +18,7 @@ import h5py
 from mo_types import MoTypes
 from Figure4 import mpath
 from scipy.signal import convolve
+from pandas import DataFrame
 
 
 # file definitions
@@ -88,6 +89,112 @@ if __name__ == "__main__":
     for i in range(all_cells_ce.shape[1]):
         all_cells_ce[:, i] = convolve(all_cells_ce[:, i], kernel, method='full')[:all_cells_ce.shape[0]]
     trial_time = np.arange(all_cells_zf.shape[0] // 3) / GlobalDefs.frame_rate
+
+    # plot colors
+    pal = sns.color_palette()  # the default matplotlib color cycle
+    plot_cols_ce = {0: pal[0], 1: pal[3], 2: pal[2], 3: pal[4], 4: pal[5],
+                    5: (0.6, 0.6, 0.6), 6: pal[6], 7: pal[1], -1: (0.6, 0.6, 0.6)}
+    step_min = 23
+    step_max = 27
+    temp_step = np.zeros(temperature.size // 3)
+    temp_step[:temp_step.size//5] = step_min
+    temp_step[temp_step.size*4//5:] = step_max
+    ramp = temp_step[temp_step.size//5:temp_step.size*4//5]
+    ramp = np.arange(ramp.size)/ramp.size*(step_max-step_min) + step_min
+    temp_step[temp_step.size//5:temp_step.size*4//5] = ramp
+    cells_ce_step = []
+    for i, p in enumerate(paths_512_ce):
+        cell_res, ids = ana_ce.temperature_activity(mpath(base_path_ce, p), temp_step, i)
+        cells_ce_step.append(cell_res)
+    cells_ce_step = np.hstack(cells_ce_step)
+    for i in range(cells_ce_step.shape[1]):
+        cells_ce_step[:, i] = convolve(cells_ce_step[:, i], kernel, method='full')[:cells_ce_step.shape[0]]
+
+    # panel - all cluster activities, sorted into ON and OFF types
+    n_regs = np.unique(clust_ids_ce).size - 1
+    cluster_acts = np.zeros((cells_ce_step.shape[0], n_regs))
+    for i in range(n_regs):
+        cluster_acts[:, i] = np.mean(cells_ce_step[:, clust_ids_ce == i], 1)
+    on_count = 0
+    off_count = 0
+    fig, (axes_on, axes_off) = pl.subplots(ncols=2, nrows=2, sharey=True, sharex=True)
+    time = np.arange(cluster_acts.shape[0]) / GlobalDefs.frame_rate
+    for i in range(n_regs):
+        act = cluster_acts[:, i]
+        if np.corrcoef(act, temp_step[:act.size])[0, 1] < 0:
+            ax_off = axes_off[0] if off_count < 2 else axes_off[1]
+            ax_off.plot(time, cluster_acts[:, i], color=plot_cols_ce[i])
+            off_count += 1
+        else:
+            ax_on = axes_on[0] if on_count < 2 else axes_on[1]
+            ax_on.plot(time, cluster_acts[:, i], color=plot_cols_ce[i])
+            on_count += 1
+    axes_off[0].set_xticks([0, 30, 60, 90, 120, 150])
+    axes_off[1].set_xticks([0, 30, 60, 90, 120, 150])
+    axes_off[0].set_xlabel("Time [s]")
+    axes_off[1].set_xlabel("Time [s]")
+    axes_on[0].set_ylabel("Cluster average activation")
+    axes_off[0].set_ylabel("Cluster average activation")
+    sns.despine()
+    fig.tight_layout()
+    fig.savefig(save_folder + "ce_all_cluster_averages.pdf", type="pdf")
+
+    # panel - average type counts in temperature branch for each cluster
+    cl_type_d = {"Fraction": [], "net_id": [], "Cluster ID": []}
+    for i in range(len(paths_512_zf)):
+        net_clust_ids = clust_ids_ce[all_ids_ce[0, :] == i]
+        for j in range(-1, n_regs):
+            cl_type_d["Fraction"].append(np.sum(net_clust_ids == j) / 1024)
+            cl_type_d["net_id"].append(i)
+            cl_type_d["Cluster ID"].append(j)
+    cl_type_df = DataFrame(cl_type_d)
+    fig, ax = pl.subplots()
+    sns.barplot("Cluster ID", "Fraction", data=cl_type_df, order=list(range(n_regs)) + [-1], ci=68, ax=ax,
+                palette=plot_cols_ce)
+    ax.set_yticks([0, 0.05, 0.1, 0.15, 0.2, 0.25])
+    sns.despine(fig, ax)
+    fig.savefig(save_folder + "ce_all_cluster_counts.pdf", type="pdf")
+
+    # panel - input connectivity into second layer of t branch
+    conn_mat = np.zeros((8, 8, len(paths_512_ce)))
+    for i, p in enumerate(paths_512_ce):
+        model_cids = clust_ids_ce[all_ids_ce[0, :] == i]
+        layers_ids = all_ids_ce[1, :][all_ids_ce[0, :] == i]
+        l_0_mask = np.full(8, False)
+        ix = model_cids[layers_ids == 0]
+        ix = ix[ix != -1]
+        l_0_mask[ix] = True
+        l_1_mask = np.full(8, False)
+        ix = model_cids[layers_ids == 1]
+        ix = ix[ix != -1]
+        l_1_mask[ix] = True
+        m_path = mpath(base_path_ce, p)
+        mdata = c.ModelData(m_path)
+        gpn = MoTypes(True).network_model()
+        gpn.load(mdata.ModelDefinition, mdata.LastCheckpoint)
+        input_result = gpn.parse_layer_input_by_cluster('t', 1, model_cids[layers_ids == 0],
+                                                        model_cids[layers_ids == 1])
+        for k, l0 in enumerate(np.arange(8)[l_0_mask]):
+            for l, l1 in enumerate(np.arange(8)[l_1_mask]):
+                conn_mat[l0, l1, i] = input_result[k, l]
+    # reordered version of conn_mat based on known types
+    cm_order = [1, 7, 0, 2, 3, 4, 5, 6]
+    cm_reordered = conn_mat[:, cm_order, :]
+    cm_reordered = cm_reordered[cm_order, :, :]
+    m = np.mean(cm_reordered, 2)
+    s = np.std(cm_reordered, 2)
+    cross_0 = np.sign((m+s) * (m-s)) <= 0
+    m[cross_0] = 0
+    s[cross_0] = 0
+    fig, axes = pl.subplots(nrows=4, sharex=True, sharey=True)
+    for i in range(4):
+        axes[i].bar(np.arange(8), m[:, i], width=[.8]*4+[.3]*4)
+        axes[i].errorbar(np.arange(8), m[:, i], s[:, i], color='k', fmt='none')
+    axes[-1].set_xticks(np.arange(8))
+    axes[-1].set_xticklabels(["AFD", "AWC/AIY", "0", "2", "3", "4", "5", "6"])
+    sns.despine(fig)
+    fig.tight_layout()
+    fig.savefig(save_folder + "ce_avg_connectivity_weights.pdf", type="pdf")
 
     # Panels: Plot best-correlated pairs of zebrafish and C. elegans neurons
     clust_corr_mat = np.zeros((np.unique(clust_ids_zf).size-1, np.unique(clust_ids_ce).size-1))
